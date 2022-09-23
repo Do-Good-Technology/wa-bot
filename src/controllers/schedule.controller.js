87379847ec080b1b396d78237c01ade88bb3ceab
baariq <baariq.fairuuz@dataon.com>
2022-09-23 16:57:53 +0700

    remove inludes 03 seconds

--- src/controllers/schedule.controller.js
+++ src/controllers/schedule.controller.js
@@ -9,18 +9,13 @@ const scheduleControllerPer60 = (client) => {
     client.sendMessage(chatIdOf('+6287738210702'), text);
 };
 
-const scheduleControllerPer1 = (client) => {
+const scheduleControllerPer1 = () => {
     const now = moment().format('HH:mm:ss');
     // const nowHour = moment().format('HH');
     // const nowMinute = moment().format('mm');
-    const nowSecond = moment().format('ss');
+    // const nowSecond = moment().format('ss');
 
     console.log('moment', now);
-    if (nowSecond === '03') {
-        console.log('masuk if 03');
-        const text = `3 seconds on ${now}`;
-        client.sendMessage(chatIdOf('+6287738210702'), text);
-    }
 };
 
 module.exports = { scheduleControllerPer1, scheduleControllerPer60 };
